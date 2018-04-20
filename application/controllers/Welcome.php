d5f1c4077f0c8afb5926fe777c93a1abbbf01156
ShaunVassallo <shaun.vassallo.a104062@mcast.edu.mt>
2018-04-20 17:18:20 +0200

    Navigation
    Navigation and pages

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,61 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends MY_Controller {
+
+	// magic method to load the parent class
+	function __construct()
+	{
+		// without this, we won't  be able to...
+		// this->build our pages.
+		parent::__construct();
+	}
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+
+	public function index(){
+
+		//this command loads a view from the views folder
+		$this->build('studentprof');
+	}
+
+	public function studentprof(){
+
+		//this command loads a view from the views folder
+		$this->build('studentprof');
+	}
+
+	public function studentres(){
+
+		//this command loads a view from the views folder
+		$this->build('studentres');
+	}
+
+	public function forms(){
+
+		//this command loads a view from the views folder
+		$this->build('forms');
+	}
+
+	public function timetables(){
+
+		//this command loads a view from the views folder
+		$this->build('timetables');
+	}
+
+
+}
---
+++ application/views/templates/start.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>MCAST</title>
+
+        <link rel="stylesheet" href="<?=base_url('css/font-awesome.css')?>">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+        <link rel="stylesheet" type="text/css" href="<?=base_url('css/style.css')?>">
+    </head>
+    <body>
+
+
+        <nav class="navbar navbar-expand navbar-dark bg-dark">
+            <ul class="navbar-nav mx-auto">
+                <?php foreach ($nav as $page => $url): ?>
+                    <li class="nav-item">
+                        <?=anchor($url, $page, array('class' => 'nav-link'));?>
+                    </li>
+                <?php endforeach ?>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="mr-auto">
+                    <a href="#" class="nav-link">Log Out</a> <!--Put logout function here-->
+                </li>
+            </ul>
+        </nav>
