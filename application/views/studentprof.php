c55484962814382ac459fdd9f102dd4613671a5a
ShaunVassallo <shaun.vassallo.a104062@mcast.edu.mt>
2018-04-24 14:39:43 +0200

    addeed in stuprof

--- application/views/studentprof.php
+++ application/views/studentprof.php
@@ -1,26 +1,28 @@
-<div class="container td-margin d-flex flex-row mt-4 mb-3 align-items-center">
+<div class="container td-margin d-flex flex-row mt-4 mb-3">
     <h5 class="card-title">Students</h5>
-    <button type="button" class="btn btn-link btn-sm mr-1"> <i class="fas fa-plus fa-sm"></i> </button>
-    <div class="dropdown mr-1">
-        <button class="btn btn-link btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Filter
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="#">Filter 1</a>
-            <a class="dropdown-item" href="#">Filter 2</a>
-            <a class="dropdown-item" href="#">Filter 3</a>
-        </div>
-    </div>
-    <button type="button" class="btn btn-link btn-sm mr-1"> <i class="fas fa-trash-alt"></i> </button>
-    <button type="button" class="btn btn-link btn-sm mr-1">Update</button>
+    <a href="#" class="btn btn-link btn-sm mr-1">
+        <i class="fas fa-plus fa-sm"></i>
+    </a>
+    <a href="#" class="btn btn-link btn-sm mr-1">
+        <i class="fas fa-trash-alt"></i>
+    </a>
+    <a href="#" class="btn btn-link btn-sm mr-1">
+        Update
+    </a>
+    <form class="form-inline my-2 my-lg-0 ml-auto">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
 </div>
 
 <div class="container">
-    <table class="table table-sm">
+    <table class="table table-sm ">
         <thead>
-            <tr class="table-active">
+            <tr class="table-active ">
                 <th scope="col">Selected</th>
-                <th scope="col">Full Name</th>
+                <th scope="col">Favorite</th>
+                <th scope="col">Name</th>
+                <th scope="col">Surame</th>
                 <th scope="col">Level</th>
                 <th scope="col">Link</th>
             </tr>
@@ -30,7 +32,9 @@
                 <th scope="row">
                     <input type="checkbox" aria-label="Checkbox for following text input">
                 </th>
+                <td> <i class="far fa-star"></i> <i class="fas fa-star yell"></i></td>
                 <td>Mark</td>
+                <td>Marks</td>
                 <td>Otto</td>
                 <td>@mdo</td>
             </tr>
