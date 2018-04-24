4b0d83e1e000c600f3c2016dee75e0964d478466
ShaunVassallo <shaun.vassallo.a104062@mcast.edu.mt>
2018-04-24 15:38:07 +0200

    renameing controllers and styel

--- application/controllers/Students.php
+++ application/controllers/Students.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends MY_Controller {
+class Students extends MY_Controller {
 
 	// magic method to load the parent class
 	function __construct()
@@ -11,35 +11,19 @@ class Welcome extends MY_Controller {
 		parent::__construct();
 	}
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-
 	public function index(){
 
 		//this command loads a view from the views folder
 		$this->build('studentport');
 	}
 
-	public function studentport(){
+	public function portfolio(){
 
 		//this command loads a view from the views folder
 		$this->build('studentport');
 	}
 
-	public function studentres(){
+	public function resources(){
 
 		//this command loads a view from the views folder
 		$this->build('studentres');
