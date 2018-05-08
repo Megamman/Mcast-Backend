0baaa4e52be85e1430f7b2069f0cf480f4382ac9
Deejay <deejay.castillo.a100646@mcast.edu.mt>
2018-05-08 13:30:55 +0200

    Updated Login Page

--- application/views/login.php
+++ application/views/login.php
@@ -10,34 +10,29 @@
         <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
         <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
-    <body class="text-centre">
-        <div class="container">
-
-
-
-            <img src="<?=base_url('images/MCAST-Main.png')?>" alt="" class="mx-auto mt-5 d-block" width="400px">
-            <form class="px-4 py-3" action="index.html" method="post">
-            <div class="mx-auto" style="width: 300px">
-                <form class="form-signin">
-                    <div class="form-group">
-                        <label for="exampleDropdownFormEmail2">Email address</label>
-                        <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleDropdownFormPassword2">Password</label>
-                        <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
-                    </div>
-                    <div class="form-check">
-                        <input type="checkbox" class="form-check-input" id="dropdownCheck2">
-                        <label class="form-check-label" for="dropdownCheck2">
-                            Remember me
-                        </label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Sign in</button>
+    <body class="text-centre body-control">
+        <div class="container wrap">
+            <div class="child">
+                <img src="<?=base_url('images/MCAST-Main.png')?>" alt="" class="mx-auto mt-5 d-block test" width="400px">
+                <form class="px-4 py-3 login-box" action="index.html" method="post">
+                <div class="mx-auto" style="width: 300px">
+                        <div class="form-group">
+                            <input type="email" class="form-control text-center" id="exampleDropdownFormEmail2" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control text-center" id="exampleDropdownFormPassword2" placeholder="Password">
+                        </div>
+                        <!--<div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="dropdownCheck2">
+                            <label class="form-check-label" for="dropdownCheck2">
+                                Remember me
+                            </label>
+                        </div>-->
+                        <button type="submit" class="btn btn-primary">Sign in</button>
+                </div>
                 </form>
             </div>
 
-            </form>
         </div>
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
