652c306e6b229fa12cb3efe2943cbf1fa9990f06
ShaunVassallo <shaun.vassallo.a104062@mcast.edu.mt>
2018-05-11 09:39:42 +0200

    added the Register

---
+++ application/views/register.php
@@ -0,0 +1,31 @@
+<form class="" action="#" method="post">
+    <div class="form-group">
+        <label for="InputIdNumb">ID Card Number</label>
+        <input type="text" class="form-control" id="InputIdNumb" aria-describedby="idnumbHelp" placeholder="Enter ID Card Number">
+    </div>
+    <div class="form-group">
+        <label for="InputEmail">Email address</label>
+        <input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+        <label for="InputPassword">Password</label>
+        <input type="password" class="form-control" id="InputPassword" placeholder="Password">
+    </div>
+    <div class="row">
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="studCheck">
+            <label class="form-check-label" for="exampleCheck1">Student</label>
+        </div>
+
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="lectCheck">
+            <label class="form-check-label" for="exampleCheck1">Lecturer</label>
+        </div>
+
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="adminCheck">
+            <label class="form-check-label" for="exampleCheck1">Admin</label>
+        </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Register</button>
+<form>
