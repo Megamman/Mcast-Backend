473b3725e2c63f446e265061b26632f08a83550a
rak3l <32982569+rak3l@users.noreply.github.com>
2018-05-11 16:21:08 +0200

    reg
    register

--- application/views/register.php
+++ application/views/register.php
@@ -1,37 +1,45 @@
 <div class="container td-margin mt-4 mb-3">
 
-    <form class="" action="#" method="post">
+    <?=form_open('Register/submit'); ?>
         <div class="form-group">
             <label for="InputIdNumb">ID Card Number</label>
-            <input type="text" class="form-control" id="InputIdNumb" aria-describedby="idnumbHelp" placeholder="Enter ID Card Number">
+            <input type="text" class="form-control" name="idcard" id="InputIdNumb" aria-describedby="idnumbHelp" placeholder="Enter ID Card Number">
+        </div>
+        <div class="form-group">
+            <label for="InputName">Name</label>
+            <input type="text" class="form-control" name="name" id="InputName" aria-describedby="nameHelp" placeholder="Enter Name">
+        </div>
+        <div class="form-group">
+            <label for="InputSurname">Surname</label>
+            <input type="text" class="form-control" name="surname" id="InputSurname" aria-describedby="surnameHelp" placeholder="Enter Surname">
         </div>
         <div class="form-group">
             <label for="InputEmail">Email Address</label>
-            <input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" placeholder="Enter email">
+            <input type="email" class="form-control" name="email" id="InputEmail" aria-describedby="emailHelp" placeholder="Enter Email">
         </div>
         <div class="form-group">
             <label for="InputPassword">Password</label>
-            <input type="password" class="form-control" id="InputPassword" placeholder="Password">
+            <input type="password" class="form-control" name="password" id="InputPassword" placeholder="Password">
         </div>
-        
+
         <label for="InputPassword">User Type</label>
         <div class="row m-auto">
 
             <div class="form-check col-auto">
-                <input type="checkbox" class="form-check-input" id="studCheck">
+                <input type="radio" class="form-check-input" id="studCheck">
                 <label class="form-check-label" for="exampleCheck1">Student</label>
             </div>
 
             <div class="form-check col-auto mx-4">
-                <input type="checkbox" class="form-check-input" id="lectCheck">
+                <input type="radio" class="form-check-input" id="lectCheck">
                 <label class="form-check-label" for="exampleCheck1">Lecturer</label>
             </div>
 
             <div class="form-check col-auto">
-                <input type="checkbox" class="form-check-input" id="adminCheck">
+                <input type="radio" class="form-check-input" id="adminCheck">
                 <label class="form-check-label" for="exampleCheck1">Admin</label>
             </div>
         </div>
         <button type="submit" class="btn btn-primary">Register</button>
-    <form>
+    <?=form_close();?>
 </div>
